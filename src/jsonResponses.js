0d18eba06890375d2fc5c0de5d7ce79899bb517f
Theory Ledbetter <trl3788@rit.edu>
2023-10-02 13:26:33 -0400

    debugging test

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -42,7 +42,7 @@ const addUser = (request, response, body) => {
 
   if (responseCode === 201) {
     responseJSON.message = 'Created Successfully';
-    respondJSON(request, response, responseCode, responseJSON);
+    //respondJSON(request, response, responseCode, responseJSON);
     return respondJSON(request, response, responseCode, responseJSON);
   }
 
